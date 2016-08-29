ded252ca5fd38229a40b5800b364fa2449bfadff
Lorin Thwaits <lorint@gmail.com>
2016-08-29 07:57:38 +0100

    Backspace and delete forward working

--- fancy_gets.rb
+++ fancy_gets.rb
@@ -3,6 +3,7 @@ require 'io/console'
 module FancyGets
 
   def gets_auto_suggest(words)
+    words.sort!
     string = ""
     position = 0
     sugg = ""
@@ -16,11 +17,11 @@ module FancyGets
         break
       when 27  # ESC -- which means lots of special stuff
         case ch = STDIN.getch.ord
-        when 79  # ESC -- which means lots of special stuff
+        when 79  # Function keys
           puts "ESC 79"
           case ch = STDIN.getch.ord
           when 80 #F1
-            puts "F1"
+            # puts "F1"
           when 81 #F2
           when 82 #F3
           when 83 #F4
@@ -32,9 +33,10 @@ module FancyGets
           when 89 #F10
           when 90 #F11
           when 91 #F12
+            # puts "F12"
           when 92 #F13
           end
-        when 91 # Special stuff
+        when 91 # Arrow keys
           case ch = STDIN.getch.ord
           when 68 # Arrow left
             if position > 0
@@ -46,25 +48,35 @@ module FancyGets
               print 27.chr + 91.chr + 67.chr
               position += 1
             end
-          # 66 - down
-          # 65 - up
-          # else
-          #   puts "ESC #{ch}"
+          when 66 # - down
+          when 65 # - up
+          when 51 # - Delete forwards?
+          else
+            puts "ESC 91 #{ch}"
           end
         else
-          # Sometimes more, like for function keys
+          # Something wacky?
           puts "code #{ch} #{STDIN.getch.ord} #{STDIN.getch.ord} #{STDIN.getch.ord} #{STDIN.getch.ord}"
         end
       when 127 # Backspace
         if position > 0
-          print "\b#{string[position..-1]} #{"\b" * (string.length - position + 1)}"
           string = string[0...position - 1] + string[position..-1]
+          prev_sugg = sugg
+          sugg = words.select { |word| word.downcase.start_with? string.downcase }.first || ""
+          extra_spaces = prev_sugg.length - sugg.length
+          extra_spaces = 0 if extra_spaces < 0
           position -= 1
+          print "\b#{string[position..-1]} - #{sugg}#{" " * extra_spaces} #{"\b" * (string.length - position + 4 + sugg.length + extra_spaces)}"
         end
       when 126 # Delete (forwards)
         if position < string.length
-          print "#{string[position + 1..-1]} #{"\b" * (string.length - position)}"
           string = string[0...position] + string[position + 1..-1]
+          prev_sugg = sugg
+          sugg = words.select { |word| word.downcase.start_with? string.downcase }.first || ""
+          extra_spaces = prev_sugg.length - sugg.length
+          extra_spaces = 0 if extra_spaces < 0
+          print "#{string[position..-1]} - #{sugg}#{" " * extra_spaces} #{"\b" * (string.length - position + 4 + sugg.length + extra_spaces)}"
+          # print "#{string[position..-1]} - #{sugg}#{" " * extra_spaces} #{"\b" * (string.length + 1 - position)}"
         end
       else # Insert character
         # print "#{ch}#{string[position..-1]}#{"\b" * (string.length - position)}"
